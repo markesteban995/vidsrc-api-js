cf019ad063aa388100784dc4b6bce25d02f32e70
Rhenzky <rmolina12865@gmail.com>
2024-11-14 16:13:04 -0700

    updates

--- src/vidsrcpro.js
+++ src/vidsrcpro.js
@@ -1,6 +1,6 @@
 export async function getvidsrc(tmdb_id, s, e) {
-  //const DOMAIN = "https://embed.su";
-  const DOMAIN = "https://vidsrc.cc";
+  const DOMAIN = "https://embed.su";
+  //const DOMAIN = "https://vidsrc.cc";
 
   const headers = {
     'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
@@ -12,9 +12,9 @@ export async function getvidsrc(tmdb_id, s, e) {
     let urlSearch = '';
 
     if(s && e){
-      urlSearch = `${DOMAIN}/v2/embed/tv/${tmdb_id}/${s}/${e}`;
+      urlSearch = `${DOMAIN}/embed/tv/${tmdb_id}/${s}/${e}`;
     } else {
-      urlSearch = `${DOMAIN}/v2/embed/movie/${tmdb_id}`;
+      urlSearch = `${DOMAIN}/embed/movie/${tmdb_id}`;
     }
     
     const htmlSearch = await fetch(urlSearch, {
@@ -46,12 +46,12 @@ export async function getvidsrc(tmdb_id, s, e) {
 
     for (let i = 0; i < secondDecode.length; i++) {
       const item = secondDecode[i];
-      /* if (item.name.toLowerCase() != "viper") {
+       if (item.name.toLowerCase() != "viper") {
         continue;
-      }  */
+      }  
 
-      //const urlDirect = `${DOMAIN}/api/e/${item.hash}`;
-      const urlDirect = `${DOMAIN}/vidplay/e/${item.hash}`;
+      const urlDirect = `${DOMAIN}/api/e/${item.hash}`;
+      //const urlDirect = `${DOMAIN}/vidplay/e/${item.hash}`;
       const dataDirect = await request_get(urlDirect, {
         "Referer": `${DOMAIN}`,
           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
@@ -93,9 +93,9 @@ export async function getvidsrc(tmdb_id, s, e) {
 
       for (let k = 0; k < parseDirectSize.length; k++) {
         const item = parseDirectSize[k];
-      /*   if (item.indexOf('/proxy/') == -1) {
+         if (item.indexOf('/proxy/') == -1) {
           continue;
-        } */
+        } 
         patternSize.push(item);
       }
 
@@ -105,7 +105,7 @@ export async function getvidsrc(tmdb_id, s, e) {
         const sizeQualityMatch = patternItem.match(/\/([0-9]+)\//i);
         const sizeQuality = sizeQualityMatch ? Number(sizeQualityMatch[1]) : 1080;
         let dURL = `${DOMAIN}${patternItem}`;
-        //dURL = dURL.replace("embed.su/api/proxy/viper/", "");
+        dURL = dURL.replace("embed.su/api/proxy/viper/", "");
         //dURL = dURL.replace("vidsrc.cc/v2/", "");
         dURL = dURL.replace(".png", ".m3u8");
         directQuality.push({
